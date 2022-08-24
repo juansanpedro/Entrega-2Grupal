06123b6177af93f3376e8b29f31420c7a976ed5a
juansanpedro <juanignacio50@hotmail.com>
2022-08-24 19:07:33 -0300

    agregar

---
+++ listado/js/almacenar.js
@@ -0,0 +1,10 @@
+const agregar = document.getElementById("agregar") 
+const limpiar = document.getElementById("limpiar") 
+const contenedor = document.getElementById("contenedor") // lista adentro de contenedor
+const item = document.getElementById("item")
+
+agregar.addEventListener("click", function(){
+
+   
+
+})
