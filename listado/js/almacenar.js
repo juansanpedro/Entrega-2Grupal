18ef84c6a97dab6482e4560c75ec682213a2f217
juansanpedro <juanignacio50@hotmail.com>
2022-08-28 21:02:56 -0300

    .

--- listado/js/almacenar.js
+++ listado/js/almacenar.js
@@ -3,13 +3,14 @@ const limpiar = document.getElementById("limpiar")
 const contenedor = document.getElementById("contenedor") // lista adentro de contenedor
 const item = document.getElementById("item")
 
+
 agregar.addEventListener("click", function(){
     if (item.value) localStorage.setItem("text", item.value);
     listado();
 
 })
 limpiar.addEventListener("click", function(){
-    localStorage.clear(contenedor);
+    localStorage.removeItem(contenedor);
 })
 
 function listado(){
