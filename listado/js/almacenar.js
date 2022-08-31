9d14a63b1b3596c5d3525e5f66cd5106e62a16e1
romicalleja <romycalleja.12.08@gmail.com>
2022-08-31 18:46:12 -0300

    wsx

--- listado/js/almacenar.js
+++ listado/js/almacenar.js
@@ -17,7 +17,7 @@ limpiar.addEventListener("click", function () {
 function listado() {
   if (item.value.length);
   contenedor.innerHTML += `<p>${item.value}</p>`;
-  array.push (`<p>${item.value}</p>`);
+  array.push (item.value ("<br></br>"));
   console.log(array);
   localStorage.setItem("array", JSON.stringify(array));
 }
