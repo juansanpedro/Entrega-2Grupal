fbd742ec73b56fdd5150cd46156193330c056810
juansanpedro <juanignacio50@hotmail.com>
2022-09-03 03:30:13 -0300

    .

--- listado/js/almacenar.js
+++ listado/js/almacenar.js
@@ -6,19 +6,19 @@ let array = [];
 
 agregar.addEventListener("click", function () {
   listado();
-  item.value = "";
+  item.value ="";
 });
 
 limpiar.addEventListener("click", function () {
   localStorage.clear();
-  contenedor.innerHTML = "";
+  contenedor.innerHTML ="";
 });
 
 function listado() {
   if (item.value.length);
-  contenedor.innerHTML += `<p>${item.value}</p>`;
-  array.push (`<br> ${item.value} `);
-  console.log(array.join(''));
+  contenedor.innerHTML +=`<p>${item.value}</p>`;
+  array.push (`<br> ${item.value}`);
+  console.log(array.join(""));
   localStorage.setItem("array", JSON.stringify(array));
 }
 document.addEventListener("DOMContentLoaded", function () {
